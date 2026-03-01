dd2f0c7660f3c15cbaa039ad8e93955dc6464686
Vermond1 <35574591+Vermond1@users.noreply.github.com>
2026-03-01 23:21:53 +0100

    Refactor DR1363Component initialization with esphome

--- components/dr1363/__init__.py
+++ components/dr1363/__init__.py
@@ -1,1 +1,4 @@
-from .component import DR1363Component
+import esphome.codegen as cg
+
+dr1363_ns = cg.esphome_ns.namespace("dr1363")
+DR1363Component = dr1363_ns.class_("DR1363Component", cg.Component)
