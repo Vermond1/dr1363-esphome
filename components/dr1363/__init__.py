a9b6b0f0f112e693c160bebc049f099cb6cea5ab
Vermond1 <35574591+Vermond1@users.noreply.github.com>
2026-03-01 23:18:51 +0100

    Refactor DR1363Component import in __init__.py

--- components/dr1363/__init__.py
+++ components/dr1363/__init__.py
@@ -1,9 +1,1 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-
-dr1363_ns = cg.esphome_ns.namespace("dr1363")
-DR1363Component = dr1363_ns.class_("DR1363Component", cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(DR1363Component),
-})
+from .component import DR1363Component
