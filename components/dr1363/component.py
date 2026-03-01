58fe5b3bc1ec30a337033af75fab0ba7b823e970
Vermond1 <35574591+Vermond1@users.noreply.github.com>
2026-03-01 22:33:43 +0100

    Refactor CONFIG_SCHEMA for DR1363Component

--- components/dr1363/component.py
+++ components/dr1363/component.py
@@ -5,18 +5,15 @@ from esphome.const import CONF_ID
 
 from . import DR1363Component
 
-
 AUTO_LOAD = ["uart"]
 
 dr1363_ns = cg.esphome_ns.namespace("dr1363")
-DR1363Component = dr1363_ns.class_("DR1363Component", cg.Component, uart.UARTDevice)
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(DR1363Component),
-}).extend(uart.UART_DEVICE_SCHEMA)
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-
