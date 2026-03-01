cdf26741464328200e7df42751cb2cc4425931b1
Vermond1 <35574591+Vermond1@users.noreply.github.com>
2026-03-01 22:23:43 +0100

    Add DR1363Component and configuration schema

--- components/dr1363/__init__.py
+++ components/dr1363/__init__.py
@@ -1,1 +1,10 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+dr1363_ns = cg.esphome_ns.namespace("dr1363")
+DR1363Component = dr1363_ns.class_("DR1363Component", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DR1363Component),
+})
 
